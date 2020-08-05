512d6df1b5de316ee6df89d1477558b03da63b93
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-05 17:56:32 -0500

    ⚔️ MVP: all 4 middlewares + PUT POST DEL endpoints (working)

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const helmet = require("helmet");
 
 const server = express();
+
+// // GLOBAL MIDDLEWARE // //
 server.use(express.json());
 server.use(helmet());
-// logger --> console {request: METHOD to URL at TIMESTAMP}
-server.use(logger);
+server.use(logger); // --> [1a]
+
+// [2]
+const userRouter = require("./users/userRouter");
+server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`hello world`);
 });
 
-//custom middleware
+module.exports = server;
 
+//custom middleware
 function logger(req, res, next) {
   // [1]
   let now = new Date().toISOString();
@@ -20,8 +26,13 @@ function logger(req, res, next) {
   next();
 }
 
-module.exports = server;
-
+// [NOTES]
 // [1]
 //  --> https://stackoverflow.com/a/30158617/14056123
 //  --> https://www.w3schools.com/jsref/jsref_obj_date.asp
+//  a. logger --> console {request: METHOD to URL at TIMESTAMP}
+//  --> before routes so that EVERY request to server is logged
+
+// [2]
+//  setting up users / posts routes
+//  --> any request to '/api/users' will be handled in './users/userRouter'
